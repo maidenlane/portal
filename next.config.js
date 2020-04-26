97eafc33591d19de7c39d3241c8fccd281ac3588
Freight Trust <4074885-freighttrust@users.noreply.gitlab.com>
2020-04-26 00:14:15 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   	webpack(config, options) {
+  		config.resolve.alias["~"] = join(__dirname);
     	return config
   	}
 });
